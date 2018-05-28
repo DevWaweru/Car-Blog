c0cd05fa875a3818cef5a348f9430ceb042b3931
Richard <richi@localhost.localdomain>
2018-05-28 08:44:20 +0300

    Deploying to heroku

--- app/email.py
+++ app/email.py
@@ -1,5 +1,5 @@
 from flask_mail import Message
-from manage import app
+# from manage import app
 from . import mail
 from flask import render_template
 import os
@@ -12,7 +12,7 @@ def send_email(subject, sender, recepients, text_body, html_body):
 
 def send_reset_email(user):
     token = user.get_reset_password_token()
-    send_email('Reset Password',sender=app.config['MAIL_USERNAME'],recepients=[user.email],text_body=render_template('admin/reset_password.txt',user=user, token=token),html_body=render_template('admin/reset_password.html',user=user, token=token))
+    send_email('Reset Password',sender=os.environ.get('MAIL_USERNAME'),recepients=[user.email],text_body=render_template('admin/reset_password.txt',user=user, token=token),html_body=render_template('admin/reset_password.html',user=user, token=token))
 
 def send_registration_email(user, pass_key):
-    send_email('Blog Contributor',sender=app.config['MAIL_USERNAME'],recepients=[user.email],text_body=render_template('emails/registration.txt',user=user, pass_key=pass_key),html_body=render_template('emails/registration.html',user=user, pass_key=pass_key))
+    send_email('Blog Contributor',sender=os.environ.get('MAIL_USERNAME'),recepients=[user.email],text_body=render_template('emails/registration.txt',user=user, pass_key=pass_key),html_body=render_template('emails/registration.html',user=user, pass_key=pass_key))
