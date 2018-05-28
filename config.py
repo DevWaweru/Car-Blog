37a745b6df91f462f149367b1acebb31c02d5f13
Richard <richi@localhost.localdomain>
2018-05-28 08:55:59 +0300

    deploying to heroku

--- config.py
+++ config.py
@@ -30,7 +30,8 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with general configuration settings
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    DEBUG = False
 
 class DevConfig(Config):
     '''
